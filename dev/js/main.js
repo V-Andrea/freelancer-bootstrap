1db0b21141334d0140a0459215bc9bdc0bb86270
T-Andrea <t.andrea.phoner@gmail.com>
2018-03-05 14:10:49 +0100

    Font Awesome Icons added

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,3 +3,19 @@ import 'bootstrap';
 require('typeface-archivo-narrow');
 require('typeface-archivo-black');
 import '../css/main.scss';
+
+import fontawesome from '@fortawesome/fontawesome';
+import faIdCard from '@fortawesome/fontawesome-free-solid/faIdCard';
+import faCartPlus from '@fortawesome/fontawesome-free-solid/faCartPlus';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+import faComments from '@fortawesome/fontawesome-free-solid/faComments';
+import faEdit from '@fortawesome/fontawesome-free-solid/faEdit';
+import faLaptop from '@fortawesome/fontawesome-free-solid/faLaptop';
+import faSpaceShuttle from '@fortawesome/fontawesome-free-solid/faSpaceShuttle';
+import faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
+import faPinterestP from '@fortawesome/fontawesome-free-brands/faPinterestP';
+import faGooglePlusG from '@fortawesome/fontawesome-free-brands/faGooglePlusG';
+
+fontawesome.library.add(faIdCard, faCartPlus, faStar, faComments, faEdit, faLaptop, faSpaceShuttle);
+fontawesome.library.add(faFacebookF, faInstagram, faPinterestP, faGooglePlusG);
